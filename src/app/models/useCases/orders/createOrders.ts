3e2c77fd4caa40d767496b0430e01c88e8ae960e
Yasuhei13 <67479901+yasuhei@users.noreply.github.com>
2022-11-17 18:55:07 -0300

    backend finalizado

---
+++ src/app/models/useCases/orders/createOrders.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { Order } from "../../Order";
+
+export async function createOrders(req: Request, res: Response) {
+  try {
+    const { table, products } = req.body;
+
+    const order = await Order.create({ table, products });
+
+    res.status(201).json(order);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+}
